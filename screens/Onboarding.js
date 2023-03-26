839a3daebf172278a6a0451f4566aff5baabe406
Javier Esneider Ibaáñez Castilla <javier.ic@icloud.com>
2023-03-25 21:41:42 -0500

    Adding Hero to Onboarding screen

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -4,6 +4,10 @@ import {
   Pressable,
   Alert,
   KeyboardAvoidingView,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { useState, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -12,7 +16,7 @@ import AppContext from "./../Providers/AppContext";
 import User from "../Providers/User";
 import CtmInputText from "./../Components/CtmInputText";
 
-import Styles from "./../Providers/Styles";
+import Styles, { gray } from "./../Providers/Styles";
 
 export default function OnboardingScreen() {
   const [firstName, setFirstName] = useState("");
@@ -57,47 +61,117 @@ export default function OnboardingScreen() {
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={{ flex: 1 }}
-        keyboardVerticalOffset={135}
+        keyboardVerticalOffset={110}
       >
-        <View
-          style={{
-            flex: 10,
-            flexDirection: "column",
-            justifyContent: "space-around",
-            padding: 20,
-          }}
-        >
-          <View
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Text style={[Styles.title, Styles.textGreen, { fontSize: 26 }]}>
-              Let us get to know you
-            </Text>
-          </View>
+        <View style={{ flex: 6 }}>
+          <ScrollView style={{ flex: 1 }}>
+            <TouchableWithoutFeedback>
+              <>
+                <View
+                  style={[
+                    HomeStyles.heroBanner,
+                    { padding: 10 },
+                    Styles.bgGreen,
+                  ]}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        Styles.textYellow,
+                        Styles.title,
+                        { fontSize: 40 },
+                      ]}
+                    >
+                      Little Lemon
+                    </Text>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 5 }}>
+                      <View>
+                        <Text style={[Styles.textWhite, Styles.title]}>
+                          Chicago
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={[Styles.textWhite, Styles.body]}>
+                          We are a family owned Mediterranean restaurant,
+                          focused on traditional recipes served whit a modern
+                          twist.
+                        </Text>
+                      </View>
+                    </View>
+                    <View style={{ flex: 4 }}>
+                      <Image
+                        source={require("./../assets/HeroImage.png")}
+                        style={HomeStyles.heroImage}
+                      />
+                    </View>
+                  </View>
+                </View>
+                <View
+                  style={[
+                    Styles.bgWhite,
+                    {
+                      flexDirection: "row",
+                      padding: 10,
+                      marginTop: 20,
+                      borderRadius: 16,
+                    },
+                  ]}
+                ></View>
+                <View
+                  style={{
+                    flex: 10,
+                    flexDirection: "column",
+                    justifyContent: "space-around",
+                    padding: 20,
+                  }}
+                >
+                  <View
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text
+                      style={[
+                        Styles.title,
+                        Styles.textGreen,
+                        { fontSize: 26, paddingBottom: 30 },
+                      ]}
+                    >
+                      Let us get to know you
+                    </Text>
+                  </View>
 
-          <View>
-            <Text style={[Styles.textGreen, { fontSize: 24 }]}>First Name</Text>
-            <CtmInputText
-              value={firstName}
-              setter={setFirstName}
-              placeholder="Type your first name"
-            ></CtmInputText>
+                  <View>
+                    <Text style={[Styles.textGreen, { fontSize: 24 }]}>
+                      First Name
+                    </Text>
+                    <CtmInputText
+                      value={firstName}
+                      setter={setFirstName}
+                      placeholder="Type your first name"
+                    ></CtmInputText>
 
-            <Text style={[Styles.textGreen, { fontSize: 24 }]}>Email</Text>
-            <CtmInputText
-              keyboardType="email-address"
-              value={email}
-              setter={setEmail}
-              placeholder="Type your email"
-            ></CtmInputText>
-          </View>
+                    <Text style={[Styles.textGreen, { fontSize: 24 }]}>
+                      Email
+                    </Text>
+                    <CtmInputText
+                      keyboardType="email-address"
+                      value={email}
+                      setter={setEmail}
+                      placeholder="Type your email"
+                    ></CtmInputText>
+                  </View>
+                </View>
+              </>
+            </TouchableWithoutFeedback>
+          </ScrollView>
         </View>
         <View
           style={{
-            flex: 2,
+            flex: 1,
             alignItems: "flex-end",
             justifyContent: "center",
             paddingHorizontal: 20,
@@ -122,3 +196,18 @@ export default function OnboardingScreen() {
     </>
   );
 }
+
+const HomeStyles = StyleSheet.create({
+  filterBox: {
+    borderBottomColor: gray,
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+  },
+  heroBanner: {},
+  heroImage: {
+    width: "100%",
+    height: 130,
+    resizeMode: "cover",
+    borderRadius: 20,
+  },
+});
